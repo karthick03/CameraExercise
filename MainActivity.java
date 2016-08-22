543c447cba86b7f65b92c004e07cee938f4e1270
Karthick <rkarthick31996@gmail.com>
2016-08-22 06:52:30 +0530

    Exercise-5a and 5b
    Added fade and zoom for image captured from camera- Android.!!!

--- MainActivity.java
+++ MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -18,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         b1=(Button)findViewById(R.id.cam);
         iv=(ImageView)findViewById(R.id.img);
-
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +27,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+    }
+    public void clockwise(View view){
+        ImageView image = (ImageView)findViewById(R.id.img);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanim);
+        image.startAnimation(animation);
+    }
+
+    public void zoom(View view){
+        ImageView image = (ImageView)findViewById(R.id.img);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        image.startAnimation(animation1);
+    }
+
+    public void fade(View view){
+        ImageView image = (ImageView)findViewById(R.id.img);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        image.startAnimation(animation1);
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
